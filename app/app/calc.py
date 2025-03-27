c9e4e9627927228a1e03b9f84b3444af8928d705
Vinitha sivaraj <vinitasivaraj.1995@gmail.com>
2025-03-27 17:12:31 +0530

    created custom commnd and creaed it inside new core app

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+# CALCULATOR 
+
+
+def add(x,y):
+
+    return x + y 
+
+    
---
+++ app/app/test.py
@@ -0,0 +1,12 @@
+from django.test import SimpleTestCase
+
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add(self):
+
+        res=calc.add(5,6)
+
+        self.assertEqual(res,11)
